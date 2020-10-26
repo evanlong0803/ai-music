0761080ab2d912ac5dffa869aeb541ea9b283b5e
123木头人 <1206993746@qq.com>
2020-10-26 22:54:08 +0800

    提交更新

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -36,10 +36,9 @@
                             <el-button round size="medium" icon="el-icon-star-off">收藏</el-button>
                         </div>
                         <div class="bottom-song">
-                            <!-- <el-table  :header-cell-style="{background:'#FAFAFA'}"> -->
                             <el-table :data="tableData" height="600" style="width: 100%" size="medium" stripe :header-cell-style="{ background: '#FAFAFA', color: '#606266' }">
                                 <el-table-column label="序号" type="index" :index="indexMethod" align="center" width="70"> </el-table-column>
-                                <el-table-column prop="date" label="歌曲" width="220"></el-table-column>
+                                <el-table-column prop="date" label="歌曲" width="220" show-overflow-tooltip></el-table-column>
                                 <el-table-column prop="name" label="歌手" width="150" show-overflow-tooltip></el-table-column>
                                 <el-table-column prop="address" label="专辑" width="180" show-overflow-tooltip></el-table-column>
                                 <el-table-column prop="date" label="时长" align="center"> </el-table-column>
@@ -50,15 +49,49 @@
             </el-col>
             <!-- 右侧 -->
             <el-col :span="7">
-                <!-- 右侧卡片 -->
+                <!-- 喜欢歌单 -->
                 <el-card class="right-card" shadow="hover">
-                    喜欢这个歌单的人
+                    <div class="card-title">喜欢这个歌单的伙伴</div>
+                    <el-row class="card-users" :gutter="10">
+                        <el-col :span="4" v-for="(item, index) in 30" :key="index">
+                            <el-image class="users-avatar" src="http://p1.music.126.net/RLeBJe4D1ZzUtltxfoKDMg==/109951163250239066.jpg?param=150y150" fit="cover"></el-image>
+                        </el-col>
+                    </el-row>
+                    <div></div>
                 </el-card>
+                <!-- 相关推荐 -->
                 <el-card class="right-card" shadow="hover">
-                    相关推荐
+                    <div class="card-title">相关推荐</div>
+                    <div class="card-featured" v-for="(item, index) in 5" :key="index">
+                        <div class="featured-left">
+                            <el-image class="featured-avatar" src="http://p1.music.126.net/RLeBJe4D1ZzUtltxfoKDMg==/109951163250239066.jpg?param=150y150" fit="cover"></el-image>
+                        </div>
+                        <div class="featured-right">
+                            <div class="featured-title">小语种民歌丨淳朴又感情的曲调</div>
+                            <div class="featured-name">By. 音乐大师师</div>
+                        </div>
+                    </div>
                 </el-card>
+                <!-- 精彩评论 -->
                 <el-card class="right-card" shadow="hover">
-                    精彩评论
+                    <div class="card-title">精彩评论</div>
+                    <div class="card-comments" v-for="(item, index) in 10" :key="index">
+                        <!-- 左侧 -->
+                        <div class="comments-left">
+                            <el-avatar :size="50" src="http://p1.music.126.net/RLeBJe4D1ZzUtltxfoKDMg==/109951163250239066.jpg?param=150y150"></el-avatar>
+                        </div>
+                        <!-- 右侧 -->
+                        <div class="comments-right">
+                            <div class="right-name">
+                                曲清酌
+                                <span>4月前</span>
+                            </div>
+                            <div class="right-content">
+                                哪有什么侧颜 阳光 白衬衫 对于普通家庭 普通学生 普通中学的我们青春的烦恼更多的是拿着不及格的试卷的不知所措 体育课800米后的汗液 班主任无意间的羞辱
+                                坐在教室的角落里望着嬉闹的他们
+                            </div>
+                        </div>
+                    </div>
                 </el-card>
             </el-col>
 
@@ -156,7 +189,9 @@ export default {
 .detail {
     width: 1200px;
     margin: 0 auto;
+    // 左边
     .left-card {
+        height: 100%;
         // 左侧上部分
         .card-top {
             .top-left-img {
@@ -227,9 +262,92 @@ export default {
             }
         }
     }
-
+    // 右边
     .right-card {
+        height: 100%;
         margin-bottom: 20px;
+        .card-title {
+            font-size: 14px;
+            font-weight: bold;
+            padding-left: 10px;
+            border-left: 3px solid #409eff;
+            margin-bottom: 10px;
+        }
+
+        // 喜欢这个歌单的人
+        .card-users {
+            .users-avatar {
+                border-radius: 5px;
+                margin: 5px 0;
+                cursor: pointer;
+            }
+        }
+
+        // 相关推荐
+        .card-featured {
+            height: 60px;
+            display: flex;
+            align-items: center;
+            margin-top: 15px;
+            // 左边
+            .featured-left {
+                height: 100%;
+                margin-right: 10px;
+                .featured-avatar {
+                    width: 60px;
+                    height: 60px;
+                    border-radius: 5px;
+                    cursor: pointer;
+                }
+            }
+            // 右边
+            .featured-right {
+                cursor: pointer;
+
+                .featured-title {
+                    font-size: 14px;
+                    font-weight: bold;
+                    margin-bottom: 10px;
+                    &:hover {
+                        color: #f56c6c;
+                    }
+                }
+                .featured-name {
+                    font-size: 12px;
+                    color: #999;
+                }
+            }
+        }
+
+        // 精彩评论
+        .card-comments {
+            height: 100%;
+            display: flex;
+            margin-top: 20px;
+            .comments-left {
+                margin-right: 12px;
+            }
+            .comments-right {
+                .right-name {
+                    font-size: 15px;
+                    font-weight: bold;
+                    margin-bottom: 10px;
+                    span {
+                        font-size: 12px;
+                        font-weight: normal;
+                        color: #999;
+                        margin-left: 5px;
+                    }
+                }
+                .right-content {
+                    background: #f5f5f5;
+                    border-radius: 5px;
+                    font-size: 12px;
+                    padding: 5px 10px;
+                    line-height: 1.6;
+                }
+            }
+        }
     }
 
     // 对话框内容
