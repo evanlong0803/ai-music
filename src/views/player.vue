0b5aa7338ad429f0b88162c46522c05a7c74ee64
123木头人 <1206993746@qq.com>
2020-10-29 15:32:33 +0800

    已实现初步播放功能

--- src/views/player.vue
+++ src/views/player.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="player">
-        <aplayer autoplay :music="list[0]" theme="#409EFF" :list="list" showLrc />
+        <aplayer autoplay :music="list[0]" theme="#409EFF" :list="list" :showLrc="true" listMaxHeight="400px" repeat="repeat-all" controls />
     </div>
 </template>
 
@@ -14,30 +14,45 @@ export default {
     },
     data() {
         return {
-            list: [
-                {
-                    title: '桃花酒',
-                    artist: '胜屿',
-                    src: 'http://m10.music.126.net/20201029091342/9e51e595948460aae3a29523de3fce4d/ymusic/050c/535a/0658/2f04f7fb7ad01b0775c012a7e962261f.mp3',
-                    pic: 'http://p1.music.126.net/GI5v3O6A7IjzxqnGNNIK1w==/109951164127239077.jpg'
-                },
-                {
-                    title: '再见曲中人',
-                    artist: '丁文涛',
-                    src: 'http://m10.music.126.net/20201029090608/4e7b323a79d244dbf02c2ad9865b1029/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/2470428796/372e/c983/0529/7acde67af7e207e40d7509d337f2a9ec.mp3',
-                    pic: 'http://p1.music.126.net/qRvVYt0AlA190xZNM7mmsQ==/109951164975688110.jpg'
-                }
-            ]
+            autoplay: true,
+            list: []
         }
     },
-    methods: {}
+    created() {
+        this.list[0] = {
+            title: '再见曲中人',
+            artist: '丁文涛',
+            src: 'http://m10.music.126.net/20201029145711/8049864f823e116d0fd7c40bb60d286b/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/2470428796/372e/c983/0529/7acde67af7e207e40d7509d337f2a9ec.mp3',
+            pic: 'http://p1.music.126.net/qRvVYt0AlA190xZNM7mmsQ==/109951164975688110.jpg'
+        }
+    },
+    mounted() {
+        // 接收当前单曲歌词
+        this.$root.$on('getSingle', Single => {
+            // 如果单歌曲已经添加到列表就停止
+            for (const i in this.list) {
+                if (this.list[i].id === Single.id) {
+                    return this.$message.success('列表已存在')
+                }
+            }
+            this.list.unshift(Single)
+        })
+
+        // 接收当前歌单所有歌曲
+        this.$root.$on('getAllSong', allSong => {
+            // allSong.forEach(item => {
+            //     this.list.push(item)
+            // })
+            this.list = allSong
+        })
+    }
 }
 </script>
 
 <style lang="less">
 .player {
     position: fixed;
-    bottom: 30%;
+    bottom: 0;
     width: 320px;
 }
 </style>
