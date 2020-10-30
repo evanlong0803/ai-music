3476fe77eda83ff72ea726602de5e19225192196
123木头人 <1206993746@qq.com>
2020-10-30 19:15:13 +0800

    提交更新

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,20 +23,18 @@
                     </el-col>
                     <el-col>
                         <div class="home-login">
-                            <!-- 搜索图标 -->
-                            <i class="el-icon-search" v-show="showSearch" @click="showSearch = !showSearch"></i>
                             <!-- 搜索框 -->
                             <el-autocomplete
                                 prefix-icon="el-icon-search"
+                                placeholder="搜索歌曲"
                                 size="medium"
                                 clearable
-                                v-model="state2"
                                 :fetch-suggestions="querySearch"
-                                placeholder="搜索歌曲"
-                                :trigger-on-focus="false"
                                 @select="handleSelect"
                             ></el-autocomplete>
+                            <!-- 分割线 -->
                             <el-divider direction="vertical"></el-divider>
+                            <!-- 登录按钮 -->
                             <el-button type="primary" size="small" @click="openLogin = true" v-if="!loginStatus">登录</el-button>
                             <!-- 已经登录显示个人信息 -->
                             <el-dropdown trigger="click" v-else>
@@ -95,14 +93,19 @@ export default {
         return {
             loginStatus: false,
             openLogin: false,
-            userInfo: {}
-            // 是否显示搜索图标
-            // showSearch: true
+            userInfo: {},
+            // 搜索内容
+            searchContent: '',
+            restaurants: [],
+            timeout: null
         }
     },
     created() {
         // 页面刷新重新加载用户信息
         this.getUserInfo()
+    },
+    mounted() {
+        this.restaurants = this.loadAll()
     },
     methods: {
         // 登录成功后，获取用户信息（Login组件登录后触发该函数）
@@ -131,12 +134,21 @@ export default {
             this.userInfo = {}
             this.loginStatus = false
         },
+        // 点击Logo跳转首页
         goHome() {
             this.$router.push('/')
         },
+        loadAll() {
+            // const { data: res } = await this.$axios.get('/search/hot/detail')
+            // console.log(res)
+            return [
+                { value: '三全鲜食（北新泾店）', address: '长宁区新渔路144号' },
+                { value: 'Hot honey 首尔炸鸡（仙霞路）', address: '上海市长宁区淞虹路661号' }
+            ]
+        },
         querySearch(queryString, cb) {
-            var restaurants = this.restaurants
-            var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants
+            let restaurants = this.restaurants
+            let results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants
             // 调用 callback 返回建议列表的数据
             cb(results)
         },
@@ -145,18 +157,9 @@ export default {
                 return restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
             }
         },
-        loadAll() {
-            return [
-                { value: '三全鲜食（北新泾店）', address: '长宁区新渔路144号' },
-                { value: 'Hot honey 首尔炸鸡（仙霞路）', address: '上海市长宁区淞虹路661号' },
-                { value: '新旺角茶餐厅', address: '上海市普陀区真北路988号创邑金沙谷6号楼113' },
-                { value: '泷千家(天山西路店)', address: '天山西路438号' },
-                { value: '胖仙女纸杯蛋糕（上海凌空店）', address: '上海市长宁区金钟路968号1幢18号楼一层商铺18-101' },
-                { value: '贡茶', address: '上海市长宁区金钟路633号' },
-                { value: '豪大大香鸡排超级奶爸', address: '上海市嘉定区曹安公路曹安路1685号' },
-                { value: '茶芝兰（奶茶，手抓饼）', address: '上海市普陀区同普路1435号' },
-                { value: '十二泷町', address: '上海市北翟路1444弄81号B幢-107' }
-            ]
+        handleSelect(item) {
+            // this.searchContent = item
+            console.log(item)
         }
     }
 }
@@ -224,7 +227,7 @@ export default {
 
                 // 分隔线
                 .el-divider--vertical {
-                    margin: 0 13px;
+                    margin: 0 15px;
                 }
 
                 // 个人信息样式
