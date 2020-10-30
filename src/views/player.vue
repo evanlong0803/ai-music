b53abf0f47af1a7838f54cce42fd734ff41588eb
123木头人 <1206993746@qq.com>
2020-10-30 22:44:30 +0800

    优化播放功能

--- src/views/player.vue
+++ src/views/player.vue
@@ -17,67 +17,77 @@ export default {
     // 页面渲染完成后开始监听
     mounted() {
         // 接收首页新歌单曲
-        this.$root.$on('getNewSong', newSong => {
-            // 如果已经添加到列表就停止
-            for (const i in this.list) {
-                // 如果在列表中发现有重复的歌曲ID
-                if (this.list[i].id === newSong.id) {
-                    // 切换到对应名称的歌曲
-                    this.$refs.aplayer.switch(newSong.name)
-                    // 立即播放
-                    this.$refs.aplayer.play()
-                    return this.$notify({
-                        title: '消息',
-                        message: `正在播放《${newSong.name}》`,
-                        type: 'success',
-                        position: 'top-left'
-                    })
-                }
-            }
-            this.list.unshift(newSong)
-            this.$refs.aplayer.switch(0) // 切换到播放列表中的第一首歌
-            this.$refs.aplayer.play() // 开始播放
-            this.$notify({
-                title: '消息',
-                message: `正在播放《${newSong.name}》`,
-                type: 'success',
-                position: 'top-left'
-            })
-        })
-
+        this.getNewSong()
         // 接收详情页单曲
-        this.$root.$on('getSingle', Single => {
-            // 如果已经添加到列表就停止
-            for (const i in this.list) {
-                // 如果在列表中发现有重复的歌曲ID
-                if (this.list[i].id === Single.id) {
-                    // 切换到对应名称的歌曲
-                    this.$refs.aplayer.switch(Single.name)
-                    // 立即播放
-                    this.$refs.aplayer.play()
-                    return this.$notify({
-                        title: '消息',
-                        message: `正在播放《${Single.name}》`,
-                        type: 'success',
-                        position: 'top-left'
-                    })
-                }
-            }
-            this.list.unshift(Single)
-            this.$refs.aplayer.switch(0) // 切换到播放列表中的第一首歌
-            this.$refs.aplayer.play() // 开始播放
-            this.$notify({
-                title: '消息',
-                message: `正在播放《${Single.name}》`,
-                type: 'success',
-                position: 'top-left'
-            })
-        })
-
+        this.getSingle()
         // 接收当前歌单所有歌曲
-        this.$root.$on('getAllSong', allSong => {
-            this.list = allSong
-        })
+        this.getAllSong()
+    },
+    methods: {
+        // 接收首页新歌单曲
+        getNewSong() {
+            this.$root.$on('getNewSong', newSong => {
+                for (const i in this.list) {
+                    // 如果在列表中发现有重复的歌曲ID
+                    if (this.list[i].id === newSong.id) {
+                        // 切换到对应名称的歌曲
+                        this.$refs.aplayer.switch(newSong.name)
+                        // 立即播放
+                        this.$refs.aplayer.play()
+                        return this.$notify({
+                            title: '消息',
+                            message: `正在播放《${newSong.name}》`,
+                            type: 'success',
+                            position: 'top-left'
+                        })
+                    }
+                }
+                this.list.unshift(newSong)
+                this.$refs.aplayer.switch(0) // 切换到播放列表中的第一首歌
+                this.$refs.aplayer.play() // 开始播放
+                this.$notify({
+                    title: '消息',
+                    message: `正在播放《${newSong.name}》`,
+                    type: 'success',
+                    position: 'top-left'
+                })
+            })
+        },
+        // 接收详情页单曲
+        getSingle() {
+            this.$root.$on('getSingle', Single => {
+                for (const i in this.list) {
+                    // 如果在列表中发现有重复的歌曲ID
+                    if (this.list[i].id === Single.id) {
+                        // 切换到对应名称的歌曲
+                        this.$refs.aplayer.switch(Single.name)
+                        // 立即播放
+                        this.$refs.aplayer.play()
+                        return this.$notify({
+                            title: '消息',
+                            message: `正在播放《${Single.name}》`,
+                            type: 'success',
+                            position: 'top-left'
+                        })
+                    }
+                }
+                this.list.unshift(Single)
+                this.$refs.aplayer.switch(0) // 切换到播放列表中的第一首歌
+                this.$refs.aplayer.play() // 开始播放
+                this.$notify({
+                    title: '消息',
+                    message: `正在播放《${Single.name}》`,
+                    type: 'success',
+                    position: 'top-left'
+                })
+            })
+        },
+        // 接收当前歌单所有歌曲
+        getAllSong() {
+            this.$root.$on('getAllSong', allSong => {
+                this.list = allSong
+            })
+        }
     }
 }
 </script>
