e8197e5f05f6b67ec032852d4c25396aaf8b4ca4
123木头人 <1206993746@qq.com>
2020-11-01 00:01:47 +0800

    搜索页面开始接收数据

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,27 @@
 <template>
-    <div></div>
+    <div>
+        搜索页面
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {}
+    },
+    created() {
+        // 获取搜索结果
+        this.getSearchRes()
+    },
+    methods: {
+        // 获取搜索结果
+        getSearchRes() {
+            this.$root.$on('getSearchRes', SearchRes => {
+                console.log(SearchRes)
+            })
+        }
+    }
+}
 </script>
 
 <style></style>
