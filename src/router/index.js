4ca3e22762db98da814c4b955fb8d2be78bfe104
123木头人 <1206993746@qq.com>
2020-11-17 22:42:39 +0800

    详情页更换为懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,6 @@ import SongSheet from '../components/SongSheet.vue';
 import Singer from '../components/Singer.vue';
 import MV from '../components/MV.vue';
 import Search from '../components/Search.vue';
-import SongDetail from '../components/detail/SongDetail.vue';
-import AlbumDetail from '../components/detail/AlbumDetail.vue';
-import VideoDetail from '../components/detail/VideoDetail.vue';
 import Personal from '../components/user/Personal.vue';
 import Grade from '../components/user/Grade.vue';
 import Setting from '../components/user/Setting.vue';
@@ -21,13 +18,14 @@ const routes = [
     { path: '/songsheet', name: 'SongSheet', component: SongSheet },
     { path: '/singer', name: 'Singer', component: Singer },
     { path: '/mv', name: 'MV', component: MV },
-    { path: '/search', name: 'Search', component: Search },
-    { path: '/songdetail', name: 'SongDetail', component: SongDetail },
-    { path: '/albumdetail', name: 'AlbumDetail', component: AlbumDetail },
-    { path: '/videodetail', name: 'VideoDetail', component: VideoDetail },
     { path: '/personal', name: 'Personal', component: Personal },
     { path: '/grade', name: 'Grade', component: Grade },
-    { path: '/setting', name: 'Setting', component: Setting }
+    { path: '/setting', name: 'Setting', component: Setting },
+    { path: '/search', name: 'Search', component: Search },
+    { path: '/songdetail', name: 'SongDetail', component: () => import('../components/detail/SongDetail.vue') },
+    { path: '/albumdetail', name: 'AlbumDetail', component: () => import('../components/detail/AlbumDetail.vue') },
+    { path: '/videodetail', name: 'VideoDetail', component: () => import('../components/detail/VideoDetail.vue') },
+    { path: '/singerdetail', name: 'SingerDetail', component: () => import('../components/detail/SingerDetail.vue') }
 ];
 
 const router = new VueRouter({
