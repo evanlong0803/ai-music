8e8d4f76e5deb1e41683d2532fc9c9380b5913f8
123木头人 <1206993746@qq.com>
2020-11-18 16:27:41 +0800

    移除多余ID传参

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -244,7 +244,6 @@ export default {
             // 重新定义播放器对象结构
             let allSong = this.single.map(item => {
                 return {
-                    id: item.id,
                     name: item.name,
                     artist: item.ar[0].name,
                     url: item.url,
