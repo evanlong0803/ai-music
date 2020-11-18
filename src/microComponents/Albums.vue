9b49f65928e62e845f5a3821636f6790a99f58fb
123木头人 <1206993746@qq.com>
2020-11-18 17:03:34 +0800

    专辑抽离为自定义组件

---
+++ src/microComponents/Albums.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-row type="flex" :gutter="30" style="flex-flow: row wrap;" v-loading="!album.length">
+        <el-col :span="4" v-for="(item, index) in album" :key="index">
+            <div class="search-album" @click="goDetail(item.id)">
+                <!-- 播放统计 -->
+                <el-tag><i class="el-icon-caret-right"></i>{{ item.type }}</el-tag>
+                <el-image class="album-img" :src="item.picUrl + '?param=175y175'" fit="cover">
+                    <div slot="placeholder" class="image-slot"></div>
+                    <div slot="error" class="image-slot"></div>
+                </el-image>
+                <div class="album-name">{{ item.name }}</div>
+                <div class="album-info">{{ item.artist.name }}</div>
+                <div class="album-info">{{ item.publishTime | timeStampTwo }}</div>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+    props: ['album'],
+    methods: {
+        // 跳转专辑详情
+        goDetail(id) {
+            this.$router.push(`/albumdetail?id=${id}`);
+        }
+    }
+};
+</script>
+
+<style></style>
