58b4ff17610882a00c76ade33a6f536e766340ab
123木头人 <1206993746@qq.com>
2020-11-18 16:29:45 +0800

    完成上部分数据渲染

--- src/components/detail/SingerDetail.vue
+++ src/components/detail/SingerDetail.vue
@@ -4,56 +4,167 @@
         <div class="detail-top">
             <div class="top-background"></div>
             <div class="top-avatar">
-                <el-avatar :size="115" :src="circleUrl"></el-avatar>
-                <div>华晨宇 <i class="el-icon-female"></i></div>
-                <el-button round plain icon="el-icon-plus" size="medium">关注ta</el-button>
+                <el-avatar :size="115" :src="singerInfo.img1v1Url"></el-avatar>
+                <div>
+                    <i v-if="userDetail.level" :class="`iconfont icon-icon-test${userDetail.level}`"></i>
+                    {{ singerInfo.name }}
+                    <i class="el-icon-female"></i>
+                </div>
+                <el-button round plain icon="el-icon-plus" size="medium" @click="follow()">关注ta</el-button>
             </div>
-            <div class="top-introduce">
-                华晨宇，1990年2月7日生于湖北十堰 ，中国男歌手，毕业于武汉音乐学院。 2013年，参加湖南卫视《快乐男声》获年度总冠军出道
-                。2014年1月，首登央视春晚舞台。9月6日-7日，在北京万事达中心连开两场“火星”演唱会 ，随后首张个...
+            <div class="top-profile">
+                {{ singerInfo.briefDesc }}
             </div>
             <div class="top-info">
                 <span>
-                    <div class="info-num">218</div>
+                    <div class="info-num">{{ singerInfo.musicSize }}</div>
                     <div>单曲数</div>
                 </span>
                 <span>
-                    <div class="info-num">35</div>
+                    <div class="info-num">{{ singerInfo.albumSize }}</div>
                     <div>专辑数</div>
                 </span>
                 <span>
-                    <div class="info-num">52</div>
+                    <div class="info-num">{{ singerInfo.mvSize }}</div>
                     <div>MV</div>
                 </span>
-                <span>
-                    <div class="info-num">6041705</div>
+                <span v-if="(userDetail.profile || {}).followeds">
+                    <div class="info-num">{{ (userDetail.profile || {}).followeds | playCount }}</div>
                     <div>粉丝</div>
                 </span>
             </div>
-            <img src="../../assets/image/arrow-lr.5aa8d800.png" />
+            <img class="top-modification" src="../../assets/image/arrow-lr.5aa8d800.png" />
         </div>
         <div class="detail-bottom">
             <el-tabs v-model="activeName" @tab-click="handleClick" stretch>
-                <el-tab-pane label="作品" name="作品">作品</el-tab-pane>
-                <el-tab-pane label="专辑" name="专辑">专辑</el-tab-pane>
+                <el-tab-pane label="作品" name="works">
+                    <el-button class="single-allPlay" type="primary" round size="medium" icon="el-icon-caret-right" @click="allPlay">
+                        播放全部
+                    </el-button>
+                    <!-- 播放列表 -->
+                    <PlayList :single="single" />
+                </el-tab-pane>
+                <el-tab-pane label="专辑" name="album">专辑</el-tab-pane>
                 <el-tab-pane label="MV" name="MV">MV</el-tab-pane>
-                <el-tab-pane label="歌手简介" name="歌手简介">歌手简介</el-tab-pane>
-                <el-tab-pane label="相似歌手" name="相似歌手">相似歌手</el-tab-pane>
+                <el-tab-pane label="歌手简介" name="profile">歌手简介</el-tab-pane>
+                <el-tab-pane label="相似歌手" name="similar">相似歌手</el-tab-pane>
             </el-tabs>
         </div>
     </div>
 </template>
 
 <script>
+import PlayList from '../../microComponents/PlayList';
 export default {
+    components: {
+        PlayList
+    },
     data() {
         return {
-            activeName: '作品'
+            activeName: 'works',
+            // 用户详情
+            userDetail: {},
+            // 歌手信息
+            singerInfo: {},
+            // 歌手单曲
+            single: []
         };
     },
+    created() {
+        let cookie = localStorage.getItem('cookie');
+        // 加载用户详情
+        this.loadUserDetail(cookie);
+
+        // 加载歌手单曲
+        this.loadSingerSingle();
+    },
     methods: {
+        // 加载用户详情
+        async loadUserDetail(cookie) {
+            try {
+                const { data: res } = await this.$axios.get('/user/detail', {
+                    params: {
+                        uid: this.$route.query.accountId,
+                        cookie
+                    }
+                });
+                if (res.code !== 200) {
+                    return this.$message.error('请求失败');
+                }
+                this.userDetail = res;
+            } catch (error) {
+                return;
+            }
+        },
+        // 加载歌手单曲
+        async loadSingerSingle() {
+            const { data: res } = await this.$axios.get('/artists', {
+                params: {
+                    id: this.$route.query.id
+                }
+            });
+            if (res.code !== 200) {
+                return this.$message.error('请求失败');
+            }
+            this.singerInfo = res.artist;
+            this.single = res.hotSongs;
+            // 定义音乐ID
+            let singleID = res.hotSongs.map(item => {
+                return item.id;
+            });
+            // 加载音乐URL
+            this.loadMusicURL(singleID);
+        },
+        // 关注
+        follow() {
+            // 没有登录
+            let cookie = localStorage.getItem('cookie');
+            // 取不到就停止
+            if (!cookie) {
+                return this.$notify({
+                    title: '消息',
+                    message: '请先登录',
+                    type: 'warning',
+                    position: 'top-left'
+                });
+            }
+        },
         handleClick(tab) {
             console.log(tab.name);
+        },
+        // 加载音乐URL
+        async loadMusicURL(singleID) {
+            const { data: res } = await this.$axios.get(`/song/url?id=${singleID.join(',')}`);
+            if (res.code !== 200) {
+                return this.$message.error('音乐URL请求失败');
+            }
+            // 储存每首音乐的RUL
+            let musicURL = res.data.map(item => {
+                return { url: item.url, id: item.id };
+            });
+            // 将每一个音乐URL放入对象属性中
+            for (const i in musicURL) {
+                for (const j in this.single) {
+                    // 如果歌单与歌单URL的ID一致，就把URL加入到对应的歌单中
+                    if (this.single[j].id === musicURL[i].id) {
+                        this.$set(this.single[j], 'url', musicURL[i].url);
+                    }
+                }
+            }
+        },
+        // 全部播放
+        allPlay() {
+            // 重新定义播放器对象结构
+            let allSong = this.single.map(item => {
+                return {
+                    name: item.name,
+                    artist: item.ar[0].name,
+                    url: item.url,
+                    cover: item.al.picUrl
+                };
+            });
+            // 传递当前歌单所有歌曲
+            this.$root.$emit('updata:getAllSong', allSong);
         }
     }
 };
@@ -84,10 +195,16 @@ export default {
         .top-avatar {
             text-align: center;
             margin-bottom: 20px;
+            position: relative;
             > div {
-                margin: 15px 0;
+                margin: 20px 0;
                 font-size: 20px;
                 font-weight: 700;
+                .iconfont {
+                    color: #e49f39;
+                    font-size: 20px;
+                    margin-right: 5px;
+                }
                 i {
                     color: #409eff;
                     font-weight: bolder;
@@ -101,9 +218,8 @@ export default {
                 }
             }
         }
-        .top-introduce {
+        .top-profile {
             width: 750px;
-            height: 50px;
             display: -webkit-box;
             -webkit-box-orient: vertical;
             -webkit-line-clamp: 2;
@@ -112,11 +228,12 @@ export default {
             color: #c5c2c5;
             line-height: 1.7;
             font-size: 14px;
+            text-align: center;
             margin-bottom: 20px;
         }
         .top-info {
             display: flex;
-            color: #c5c2c5;
+            color: #a29aa1;
             line-height: 1.7;
             font-size: 14px;
             margin-bottom: 50px;
@@ -129,7 +246,7 @@ export default {
                 color: white;
             }
         }
-        img {
+        .top-modification {
             width: 100%;
             position: absolute;
             bottom: 0;
@@ -143,6 +260,11 @@ export default {
             .el-tabs__nav-scroll {
                 width: 50%;
             }
+        }
+        // 单曲全部播放
+        .single-allPlay {
+            float: right;
+            margin-bottom: 20px;
         }
     }
 }
