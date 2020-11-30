3ff422dcc54353bf6aa629d9edba1075b7de0b48
123木头人 <1206993746@qq.com>
2020-11-30 13:32:48 +0800

    修复BUG

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     //打包是否生成.map文件
-    productionSourceMap: false
-}
+    productionSourceMap: false,
+    //修改或新增html-webpack-plugin的值，在index.html里面能读取htmlWebpackPlugin.options.title
+    chainWebpack: config => {
+        config.plugin('html').tap(item => {
+            console.log(item);
+            item[0].title = 'Cookies-Music';
+            return item;
+        });
+    }
+};
