d5da58e4fca0c869fc582b26a70aa300979adb6b
123木头人 <1206993746@qq.com>
2021-01-04 13:25:50 +0800

    2021年1月4日13:25:49

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     //打包是否生成.map文件
     productionSourceMap: false,
     // 判断环境：生产环境production、开发环境development
-    publicPath: process.env.NODE_ENV === 'production' ? './dist/' : './',
+    publicPath: process.env.NODE_ENV === 'production' ? '../' : './',
     //修改或新增html-webpack-plugin的值，在index.html里面能读取htmlWebpackPlugin.options.title
     chainWebpack: config => {
         config.plugin('html').tap(item => {
