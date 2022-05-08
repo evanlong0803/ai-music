89d97a8e34548df63315bcee77f27a158032ce70
123木头人 <1206993746@qq.com>
2022-05-08 19:59:29 +0800

    🌈 style(router): 格式化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 // vite-env.d.ts
 /// <reference types="vite-plugin-pages/client" />
 
-import routes from '~pages'
+import routes from '~pages';
+
 import { createRouter, createWebHistory } from 'vue-router';
 
 export default createRouter({
-	history: createWebHistory(),
-	routes,
+  history: createWebHistory(),
+  routes,
 });
