5f6bfd4ad0db8ee03d5922b66d7cfe60732268e7
123木头人 <1206993746@qq.com>
2022-05-08 20:58:43 +0800

    🌈 style(工具函数): 修改函数名称

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const handleplayCount = (target: number) => {
+export const handlePlayCount = (target: number) => {
   const numStr = target.toString();
   // 4位数以内直接返回
   if (numStr.length <= 4) return target + '+';
