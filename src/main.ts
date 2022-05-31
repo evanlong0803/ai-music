d8f47448283b1bdb21f6ae7c309b903da0af49be
123木头人 <1206993746@qq.com>
2022-06-01 00:10:18 +0800

    🦄 refactor(main): 调整css文件引入顺序

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
-import '@/styles/index.less';
 
 // import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
-import 'virtual:windi.css';
+import 'virtual:windi-base.css';
+import 'virtual:windi-components.css';
+import '@/styles/index.css';
+import 'virtual:windi-utilities.css';
 import 'virtual:windi-devtools';
 
 import App from './App.vue';
 import router from '@/router';
-import store from '@/store';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).use(createPinia()).mount('#app');
