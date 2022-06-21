261f3b7450be1ff1d517351ba2eb357739e9b0cb
123木头人 <1206993746@qq.com>
2022-06-21 23:33:37 +0800

    🌈 style(main): 移除注释

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 
-// import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import 'virtual:windi-base.css';
 import 'virtual:windi-components.css';
 import '@/styles/index.css';
